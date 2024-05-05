1d8a8afdccdc03d7bc30b917f847c8750685db7a
Jahanzaib00 <jahanzaibkhursheed2002@gmail.com>
2024-05-06 03:10:27 +0500

    Image feature is implemented

--- frontend/src/storyDisplay.js
+++ frontend/src/storyDisplay.js
@@ -4,7 +4,8 @@ import { useLocation, useNavigate } from "react-router-dom";
 function StoryDisplay() {
   const location = useLocation();
   const navigate = useNavigate();
-  const { story, image } = location.state;
+  const { data } = location.state;
+  console.log(data);
 
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
@@ -12,19 +13,21 @@ function StoryDisplay() {
         <h1 className="text-2xl font-bold text-center">
           Generated Story and Image
         </h1>
-        <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0 md:space-x-4">
-          <div className="md:w-1/2">
-            <h2 className="text-lg font-semibold">Story</h2>
-            <p className="text-gray-700">{story}</p>
+        {data.map((item) => (
+          <div className="flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0 md:space-x-4">
+            <div className="md:w-1/2">
+              <h2 className="text-lg font-semibold">Story</h2>
+              <p className="text-gray-700">{item.story}</p>
+            </div>
+            <div className="md:w-1/2">
+              <img
+                src={`data:image/png;base64, ${item.image}`}
+                alt="Generated"
+                className="max-w-full h-auto rounded-lg shadow-lg"
+              />
+            </div>
           </div>
-          <div className="md:w-1/2">
-            <img
-              src={image}
-              alt="Generated"
-              className="max-w-full h-auto rounded-lg shadow-lg"
-            />
-          </div>
-        </div>
+        ))}
         <button
           onClick={() => navigate("/")}
           className="mt-4 py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-700"
--- frontend/src/storyGenerator.js
+++ frontend/src/storyGenerator.js
@@ -9,6 +9,7 @@ function StoryGenerator() {
     event: "War",
     place: "Warzone",
   });
+  const [data, setData] = useState([]);
   const [generateButton, setGenerateButton] = useState("Generate Story");
   const navigate = useNavigate();
 
@@ -21,8 +22,15 @@ function StoryGenerator() {
     e.preventDefault();
     setGenerateButton("Generating...");
     const storyData = await generateStory(inputs);
+    // const storyData = {
+    //   story:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam recusandae alias error harum maxime adipisci amet laborum. Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero magni deleniti quod quam consequuntur! Commodi minima excepturi repudiandae velit hic maxime doloremque. Quaerat provident commodi consectetur veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam cupiditate excepturi mollitia maiores labore suscipit quas? Nulla, placeat. Voluptatem quaerat non architecto ab laudantium modi minima sunt esse temporibus sint culpa, recusandae aliquam numquam totam ratione voluptas quod exercitationem fuga. Possimus quis earum veniam quasi aliquam eligendi, placeat qui corporis!",
+    //   image:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6ESbW-tVWx6ohOQYEfDBU0kfBLm2WrVroj-7EAf6zaQ&s",
+    // };
+    setData((prevData) => [...prevData, storyData]);
     // Pass storyData via state to the /story route
-    navigate("/story", { state: { ...storyData } });
+    navigate("/story", { state: { data: [...data, storyData] } });
     setGenerateButton("Generate Story");
   };
 
@@ -87,7 +95,7 @@ function StoryGenerator() {
           <button
             type="submit"
             className="w-full text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            disabled={generateButton == "Generating..."}
+            disabled={generateButton === "Generating..."}
           >
             {generateButton}
           </button>
